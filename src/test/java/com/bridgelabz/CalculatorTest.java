dd8923d74c0d9f843c83159e4d6d90537aa94985
Nikhil Rawat <nikhilrawaa311@gmail.com>
2025-04-04 13:15:47 +0530

    [Nikhil] Mod: modified calculator and stringUtilityMethods class

--- src/test/java/com/bridgelabz/CalculatorTest.java
+++ src/test/java/com/bridgelabz/CalculatorTest.java
@@ -11,25 +11,25 @@ class CalculatorTest {
     private final Calculator calculator = new Calculator();
 
     @Test
-    void add() {
+    void shouldAddElements() {
         assertEquals(5, calculator.add(2, 3));
         assertEquals(-1, calculator.add(-2, 1));
     }
 
     @Test
-    void subtract() {
+    void shouldSubtractElements() {
         assertEquals(1, calculator.subtract(3, 2));
         assertEquals(-3, calculator.subtract(-2, 1));
     }
 
     @Test
-    void multiply() {
+    void shouldMultiplyElements() {
         assertEquals(6, calculator.multiply(2, 3));
         assertEquals(0, calculator.multiply(0, 5));
     }
 
     @Test
-    void divide() {
+    void shouldDivideElements() {
         assertEquals(2, calculator.divide(6, 3));
         assertEquals(-2, calculator.divide(-6, 3));
     }
--- src/test/java/com/bridgelabz/StringUtilityMethodsTest.java
+++ src/test/java/com/bridgelabz/StringUtilityMethodsTest.java
@@ -6,24 +6,29 @@ import org.junit.jupiter.api.Test;
 class StringUtilityMethodsTest {
 
     @Test
-    void reverse() {
+    void shouldReverse() {
         String result = StringUtilityMethods.reverse("nikhil");
         String expected = "lihkin";
-        Assertions.assertEquals(expected,result);
+        Assertions.assertEquals(expected, result);
+
+        String res = StringUtilityMethods.reverse("Nikhil   ");
+        String expected1 = "   lihkiN";
+        Assertions.assertEquals(expected1, res);
+
     }
 
     @Test
     void isPalindrome() {
         boolean result = StringUtilityMethods.isPalindrome("naman");
         boolean expectedResult = true;
-
         Assertions.assertEquals(expectedResult,result);
+
     }
 
     @Test
     void toUpperCase() {
         String result = StringUtilityMethods.toUpperCase("uppercase");
         String expected = "UPPERCASE";
-        Assertions.assertEquals(result,expected);
+        Assertions.assertEquals(expected,result);
     }
 }
