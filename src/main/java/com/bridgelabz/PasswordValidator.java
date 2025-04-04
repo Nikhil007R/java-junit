b7491cb47ee5757d12c8c8f31349aeb7fc7caf21
Nikhil Rawat <nikhilrawaa311@gmail.com>
2025-04-04 09:29:48 +0530

    [Nikhil] Add: testing password strength validator.

---
+++ src/main/java/com/bridgelabz/PasswordValidator.java
@@ -0,0 +1,14 @@
+package com.bridgelabz;
+
+public class PasswordValidator {
+
+    // Regular expression for a valid password
+    private static final String regex = "^(?=.*[A-Z])(?=.*\\d).{8,}$";
+
+    public static boolean isValidPassword(String password) {
+        if (password == null) {
+            return false;
+        }
+        return password.matches(regex);
+    }
+}
